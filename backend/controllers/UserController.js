b0af7e3dcb263b7a5e44e79912fbee679791f9ae
robertocarrazzoni <roberto.carrazzoni17@gmail.com>
2022-08-10 14:32:45 -0300

    edit e upload de imagem

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,10 +1,12 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
 //helpers
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 module.exports = class UserController {
     static async register(req, res) {
         const { name, email, phone, password, confirmpassword} = req.body
@@ -112,6 +114,105 @@ module.exports = class UserController {
             currentUser = null
         }
         res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req, res) {
+        const id = req.params.id
+
+        if (!id.match(/^[0-9a-fA-F]{24}$/)){
+            res.status(422).json({message:"Usuário não encontrado"})
+            return
+        }
+
+        const user = await User.findById(id).select('-password')
+
+        if (!user) {
+            res.status(422).json({message: 'Usuário não encontrado'})
+            return
+        }
+
+        res.status(200).json({user})
+    }
+
+    static async editUser(req, res) {
+        
+        const id = req.params.id
+
+        // check if user exists
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        if (req.file) {
+            user.image = req.file.filename
+        }
+
+        if (!id.match(/^[0-9a-fA-F]{24}$/)){
+            res.status(422).json({message:"Usuário não encontrado"})
+            return
+        }
+
+        if (!name) {
+            res.status(422).json({message: 'O nome é obrigatório'})
+            return
+        }
+
+        user.name = name
+
+        if (!email) {
+            res.status(422).json({message: 'O email é obrigatório'})
+            return
+        }
+
+        //check if email has already taken
+        const userExists = User.findOne({email: email})
+
+        if (user.email !== email && userExists) {
+            return res.status(422).json({message: 'Por favor, utilize outro email'})
+        }
+
+        user.email = email
+
+        if (!phone) {
+            res.status(422).json({message: 'O telefone é obrigatório'})
+            return
+        }
+
+        user.phone = phone
+
+        if (password !== confirmpassword) {
+
+            return res.status(422).json({message: 'As senhas não conferem'})
+
+        } else if (password === confirmpassword && password != null) {
+            //creating password
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password, salt)
+
+            user.password = passwordHash
+        }
+        
+        try {
+            
+            // returns user updated data
+            await User.findOneAndUpdate(
+                {_id: user._id},
+                {$set: user},
+                {new: true}
+            )
+            res.status(200).json({message: 'Usuário atualizado com sucesso'})
+
+        } catch (error) {
+            res.status(500).json({message: error})
+             return
+        }
+        
+
+        
+
+
     }
 
 }
